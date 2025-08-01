8ee889759b320bb45dc2ab1149ea1f799a48a154
Sabitha J <113449189+Sabitha-23@users.noreply.github.com>
2025-08-01 10:48:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const quotes = [
   { text: "The best way to get started is to quit talking and begin doing.", author: "Walt Disney" },
   { text: "Don't let yesterday take up too much of today.", author: "Will Rogers" },
-  { text: "It's not whether you get knocked down, it's whether you get up.", author: "Vince Lombardi" },
   { text: "If you are working on something exciting, it will keep you motivated.", author: "Steve Jobs" },
   { text: "Success is not in what you have, but who you are.", author: "Bo Bennett" },
-  { text: "When you really desire something, All the universe conspires in helping you to achieve it.", author: "Paulo Coelho" }
+  { text: "When you really desire something, All the universe conspires in helping you to achieve it.", author: "Paulo Coelho" },
+  { text: "It's not whether you get knocked down, it's whether you get up.", author: "Vince Lombardi" }
+  
 ];
 
 function generateQuote() {
